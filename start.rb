04335e8f5a0a0b576a8b9feae1dd122c602ed5fc
Ivan Danci <bah4uk@gmail.com>
2016-06-25 23:59:26 +0300

    Start a game

---
+++ start.rb
@@ -0,0 +1,31 @@
+require_relative "lib/nasa"
+
+puts "Input upper-right coordinates of the plateau, X and Y"
+coords = gets.chomp.scan(/\d/)[0..1]
+
+expedition = Expedition.new(*coords)
+rovers     = []
+
+["first", "second"].each do |name|
+  puts "Deploy #{name} rover, input it's coordinates and direction"
+  input     = gets.chomp
+  coords    = input.scan(/\d/)[0..1]
+  direction = input.scan(/[#{Rover::DIRECTIONS.join}]/i).last
+  expedition.deploy_rover(coords.first, coords.last, direction, name)
+
+  puts "Provide unstructions for the #{name} rover, use 'L', 'R' and 'M'"
+  input     = gets.chomp
+  rover     = expedition.find_rover(name)
+  expedition.move_rover(rover, input)
+end
+
+expedition.rovers.each do |rover|
+  puts "#{rover.position.join}#{rover.direction}"
+end
+
+# 5 5
+# 1 2 N
+# LMLMLMLMM
+
+# 3 3 E
+# MMRMMRMRRM
